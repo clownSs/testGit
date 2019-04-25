0928bf1621c9ac2be89dc67539c261e0508a76a9
eclipse_user <eclipse@123.com>
2019-04-25 09:44:57 +0800

    hot_fix提交

--- src/main/java/com/clowns/git/GitTest.java
+++ src/main/java/com/clowns/git/GitTest.java
@@ -2,5 +2,10 @@ package com.clowns.git;
 
 public class GitTest {
 
-	
+	public static void main(String[] args) {
+		System.out.println("ls提交");
+		System.out.println("zs提交");
+		System.out.println("hot_fix提交");
+		
+	}
 }
